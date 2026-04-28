91081900e4d6ef0c2eb079604ec39b45099858f9
Adrian <spottyadrian@gmail.com>
2026-04-28 12:58:06 -0300

    add: scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto baseado no tab-news, seguindo o curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-function home (){
+function home() {
+  return <h1>Oi, testando a hospedagem gratis da vercel</h1>;
+}
 
-    return <h1>Oi, testando a hospedagem gratis da vercel</h1>
+function olaMundo() {
+  console.log("ola mundo");
+}
 
-
-
+function teste() {
+  console.log("identacao errada");
 }
 export default home;
